836ba6e499be2042f0179d14d246d3d35e055e14
Avtandil Pangani <avto@telaveli.ge>
2021-12-18 22:44:58 +0400

    მენიუს ჩონჩხი

--- views.py
+++ views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+menu = [{"item":"მთავარი", "href":"home","id":0},
+        {"item":"კონტაქტი", "href":"contact","id":1}]
+
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    
+    return render(request, 'main/index.html' ,{'menu':menu})
+
+def contact(request):
+    
+    return render(request, 'main/contact.html' ,{'menu':menu})
     
