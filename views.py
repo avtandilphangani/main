3490de666ff218824c68e81ee7a3808ad8a957a1
Avtandil Pangani <avto@telaveli.ge>
2021-12-18 23:55:32 +0400

    გავასწორე მენიუ

--- views.py
+++ views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 
-menu = [{"item":"მთავარი", "href":"home","id":0},
-        {"item":"კონტაქტი", "href":"contact","id":1}]
+menu = [{"item":"მთავარი", "href":"home","id":1},
+        {"item":"კონტაქტი", "href":"contact","id":2}]
 
 # Create your views here.
 def index(request):
     
-    return render(request, 'main/index.html' ,{'menu':menu})
+    return render(request, 'main/index.html' ,{'menu':menu, "id":1})
 
 def contact(request):
     
-    return render(request, 'main/contact.html' ,{'menu':menu})
+    return render(request, 'main/contact.html' ,{'menu':menu,"id":2})
     
