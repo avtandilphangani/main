f4cbd01666f151b69f0fe003c190ff00d4f3b017
Avtandil Pangani <avto@din.ge>
2022-01-03 21:26:43 +0400

    global change

--- models.py
+++ models.py
@@ -3,15 +3,14 @@ from django.db import models
 from django.db.models.base import Model, ModelBase
 from django.db.models.fields import BooleanField
 
-# Create your models here.
 
 class MainMenu(models.Model):
     """Model definition for MainMenu."""
 
     
-    menuitem = models.CharField("Menu", max_length=50)
-    mslug = models.SlugField("Slug")
-    isvisible = models.BooleanField(default=False)
+    menu_item = models.CharField("Menu", max_length=50)
+    memu_slug = models.SlugField("Slug")
+    menu_visible = models.BooleanField(default=False)
     
     
     class Meta:
@@ -22,68 +21,14 @@ class MainMenu(models.Model):
 
     def __str__(self):
         """Unicode representation of MainMenu."""
-        return self.menuitem
+        return self.menu_item
 
     
     # TODO: ეს გასარკვევი მაქვს
     def get_absolute_url(self):
         """Return absolute url for MainMenu."""
-        return reverse(self.mslug)
+        return reverse(self.menu_slug)
 
     # TODO: Define custom methods here
     
-    
-class Gallery(models.Model):
-    """Model definition for Gallery."""
-    ph_name = models.CharField("Name", max_length=50)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    style =  models.CharField("Style",max_length = 50)
-    title = models.CharField(max_length = 255)
-    content = models.TextField(blank=True)
-    btitle = models.CharField(max_length = 150)
-    bslug = models.SlugField(max_length = 50)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateField(auto_now=True)
-    is_published = models.BooleanField(default=False)
-    is_draft = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
-    
-     
-    # TODO: Define fields here
-
-    class Meta:
-        """Meta definition for Gallery."""
-
-        verbose_name = 'Gallery'
-        verbose_name_plural = 'Gallerys'
-
-    def __str__(self):
-        """Unicode representation of Gallery."""
-        return self.ph_name
-
-    
-
-
-    # TODO: Define custom methods here
-    
-class Category(models.Model):
-    """Model definition for category."""
-
-    # TODO: Define fields here
-    name = models.CharField(max_length = 150,db_index=True)
-    
-
-    class Meta:
-        """Meta definition for category."""
-
-        verbose_name = 'category'
-        verbose_name_plural = 'categorys'
-
-    def __str__(self):
-        """Unicode representation of category."""
-        return self.name
-
-    
-
-
-    # TODO: Define custom methods here
+ 
--- views.py
+++ views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 
 from main.models import MainMenu
 
-'''menu = [{"item":"მთავარი", "href":"home","id":1},
-        {"item":"კონტაქტი", "href":"contact","id":2}]'''
+
 menu = MainMenu.objects.all()
 
-slides = [ {"img":"photo.jpg", "dark":"uk-light", "title":"თანამედროვე სახლის მოდელი", "button":"შეიტყვე მეტი", "text":"ძალიან ლამაზი ფოტოა, ხოლო ტექსტი ანიმირდება სურათთან ერთად"},
-         {"img":"light.jpg", "dark":"uk-dark", "title":"ნათელი ფერების დიდზაინი","button":"ფერების შესახებ", "text":"ტექსტი ანიმრდება და მოცემულია შრიფტით გლახო"},
-         {"img":"dark.jpg", "dark":"uk-light", "title":"მუქი სურათი","button":"ლამაზი ნაკეთობები", "text":"ტყავის ლამაზი ნაკეთობა"}]
 
 # Create your views here.
 def index(request):
